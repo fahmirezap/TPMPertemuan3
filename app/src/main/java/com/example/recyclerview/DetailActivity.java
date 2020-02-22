d3fb043301107ed6b49b55416ae15b3b54b3db63
Fahmi Reza Prasastio <frezap17@gmail.com>
2020-02-22 10:21:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/DetailActivity.java
@@ -0,0 +1,32 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+    ImageView imageview;
+    TextView textnama, textdetail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        textnama = findViewById(R.id.tvNama);
+        textdetail = findViewById(R.id.tvDetail);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String nama = bundle.getString("nama");
+        String detail = bundle.getString("detail");
+
+        textnama.setText(nama);
+        textdetail.setText(detail);
+        textdetail.setMovementMethod(new ScrollingMovementMethod());
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<modelPahlawan> modelPahlawans = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvPahlawan);
+        recyclerView.setHasFixedSize(true);
+        modelPahlawans.addAll(dataPahlawan.getListData());
+
+        showRecyclerList();
+    }
+    private void showRecyclerList() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        AdapterPahlawan adapterPahlawan = new AdapterPahlawan(this);
+        adapterPahlawan.setModelPahlawans(modelPahlawans);
+        recyclerView.setAdapter(adapterPahlawan);
+    }
+}
